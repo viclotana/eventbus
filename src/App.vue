fcc84bf79dfe6fdb7fbf531e3c527d13725c3e85
Nwose Lotanna <lotanna.nwose@meltwater.org>
2019-09-27 05:31:11 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Test v-bind:header="header"/>
+    <Test2 v-bind:header="header"/>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue';
+import Test2 from './components/Test2.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Test, Test2
+  },
+  data (){
+    return {
+      header:'initial header'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test2.vue
@@ -0,0 +1,40 @@
+<template>
+  <div> <h1>{{header}}</h1>
+  </div>
+</template>
+
+<script>
+import { bus } from '../main';
+export default {
+  name: 'Test2',
+  props: {
+    header:{
+        type: String
+    } 
+  },
+  created (){
+    bus.$on('changeIt', (data) => {
+      this.header = data;
+    })
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
